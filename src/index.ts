eec843021b25fd165d14dd347da3fc6b71a06713
iShoma <my@ishoma.com>
2020-11-07 03:37:17 +0300

    ready for publish

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,10 @@ import { verifyEngines } from './verifyEngines';
 export const run = async (): Promise<void> => {
   try {
     await verifyEngines();
+    console.log('\x1b[37m\x1b[42m', 'OK!', '\x1b[0m', 'ENGINE-VERSION check passed');
   } catch (errors) {
     errors.forEach((err: string) => {
-      console.error(err);
+      console.error('\x1b[37m\x1b[41m', 'ERROR!', '\x1b[0m', 'ENGINE-VERSION check failed:', err);
     });
     process.exit(1);
   }
